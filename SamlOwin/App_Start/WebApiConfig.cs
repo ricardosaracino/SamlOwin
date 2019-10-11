ca5d00a9802c7207129246d3e052ce35b755dbbd
RicardoSaracnio <ricardo.saracino@053gc.onmicrosoft.com>
2019-10-11 08:08:13 -0400

    inital commit

---
+++ SamlOwin/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace SamlOwin
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            RegisterFormatters(config);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        private static void RegisterFormatters(HttpConfiguration config)
+        {
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+        }
+    }
+}
+
---
+++ SamlOwin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SamlOwin.WebApiApplication" Language="C#" %>
